1583079f80a96099749b07b68e9dfcfdd2ee4808
arthurony <49954136+arthurony@users.noreply.github.com>
2024-12-10 18:48:07 -0300

    Update E-Novel1.lua

--- src/pt/E-Novel1.lua
+++ src/pt/E-Novel1.lua
@@ -1,7 +1,7 @@
 -- {"id":172008,"ver":"1.0.0","libVer":"1.0.0","author":"felipebonfim2006","dep":["Madara=2.9.0"]}
 
 return Require("Madara")("https://e-novel.online", {
-    id = 172008,
+    id = 1720081,
     name = "E-Novel1",
     imageURL = "https://e-novel.online/wp-content/uploads/2021/06/logo-novela.fw_.png",
 
